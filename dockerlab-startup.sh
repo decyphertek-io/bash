3cc841f22f35167bbc1790c763f2660d9515e801
decyphertek-io <ciphertek@protonmail.com>
2023-06-23 01:27:29 -0500

    decyphertek-io

---
+++ dockerlab-startup.sh
@@ -0,0 +1,5 @@
+# I want to have a bash script that can create an interactive docker launch system. 
+# launch portainer , then you have a work envrionment to build your own custom docker containers
+# OR to build an existing server
+# How many server categories is needed to build a functional docker build pipeline?
+# Working progress............
